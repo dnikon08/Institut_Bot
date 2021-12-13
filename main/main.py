14601c7d7e2fc7579eede183469a5dc7330806f2
dnikon08 <65525310+dnikon08@users.noreply.github.com>
2021-12-13 21:16:48 +0300

    Responses in main osnova 2.0v
    With easter eggs!

--- main/main.py
+++ main/main.py
@@ -1,12 +1,110 @@
 import logging
 import asyncio
 from aiogram.utils.exceptions import BotBlocked
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, executor, types
 from aiogram.dispatcher import Dispatcher
 from os import getenv
 from sys import exit
-import Responses as R
-import time
+import json
+import nltk
+import numpy
+import random
+import tensorflow
+import tflearn
+import pickle
+from nltk.stem.lancaster import LancasterStemmer
+
+nltk.download('punkt')
+stemmer = LancasterStemmer()
+
+with open('intents.json', encoding='utf-8') as file:
+    data = json.load(file)
+
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+tensorflow.compat.v1.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
+
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
 
 bot_token = getenv("BOT_TOKEN")
 if not bot_token:
@@ -19,10 +117,13 @@ logging.basicConfig(level=logging.INFO)
 
 print("PROGRAM START")
 # пример обработчика ошибки
+
+
 @dp.message_handler(commands="block")
 async def cmd_block(message: types.Message):
     await asyncio.sleep(10.0)  # Здоровый сон на 10 секунд
     await message.reply("Вы заблокированы")
+
 
 @dp.errors_handler(exception=BotBlocked)
 async def error_bot_blocked(update: types.Update, exception: BotBlocked):
@@ -35,7 +136,7 @@ async def error_bot_blocked(update: types.Update, exception: BotBlocked):
     return True
 
 
-@dp.message_handler(commands="start") # новый обработчик
+@dp.message_handler(commands="start")  # новый обработчик
 async def cmd_test1(message: types.Message):  # await топерь обязателен
     await message.reply("start is working")
 
@@ -44,9 +145,25 @@ async def cmd_test1(message: types.Message):  # await топерь обязат�
 async def cmd_test1(message: types.Message):
     await message.reply("help is working")
 
+
+@dp.message_handler()
+async def cmd_test1(message: types.Message):
+    inp = str(message)
+
+    results = model.predict([bag_of_words(inp, words)])[0]
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+
+    if results[results_index] > 0.7:
+        for tg in data["intents"]:
+            if tg['tag'] == tag:
+                responses = tg['responses']
+
+        await message.reply(random.choice(responses))
+    else:
+        await message.reply("Я не понял вас. Попробуйте снова")
+
+
 if __name__ == "__main__":
     # Запуск бота
     executor.start_polling(dp, skip_updates=True)
-
-
-
